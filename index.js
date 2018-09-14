893c8eba8532f24f41f0d60e7edec48fde7b077b
George Lima <georgelima11@hotmail.com>
2018-09-14 11:50:25 -0300

    Update: Add wrapper function in eres

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-module.exports = function(promise) {
-  return promise
-    .then(function(result) {
-      return [null, result]
-    })
-    .catch(function(err) {
-      return [err]
-    })
+module.exports = function(onError) {
+  return function(promise) {
+    return promise
+      .then(function(result) {
+        return [null, result]
+      })
+      .catch(function(err) {
+        if (onError) {
+          onError(err)
+        }
+        return [err]
+      })
+  }
 }
